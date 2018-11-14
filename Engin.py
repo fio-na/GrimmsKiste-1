4364946209a64c81707d57466e16bc96374414c9
fio-na <fiona.zurek@posteo.de>
2018-11-14 17:12:58 +0100

    results of testing different Popen commands with printer

--- Engin.py
+++ Engin.py
@@ -10,10 +10,10 @@ lpr = subprocess.Popen("/usr/bin/lpr", stdin=subprocess.PIPE)
 def print_to_printer(text_to_print):
     #lpr = subprocess.Popen("/usr/bin/lpr", stdin=subprocess.PIPE)
     lpr.stdin.write(text_to_print.encode("utf-8"))
+    #if lpr.communicate is used, it prints directly when it is called. however you cannot keep the pipe open, and uf you open and close it, the pater gets cut
+    #if lpr.stdin.write is used, it prints only when the program is finished, but if you open lpr in the beginning of the process you can print multiple things without the paper being cut
 
 def processState(state):
-    print_to_printer("hi")
-    print_to_printer("hallo")
     print_to_printer(state["message"])
 
     if not "actions" in state:
