98debdaadd914bf6d042b52dc40dd03ebb04b382
fio-na <fiona.zurek@posteo.de>
2018-11-16 00:39:06 +0100

    final fix to both print every time the print_to_printer function is called and not cut the paper until the end of the program

--- Engin.py
+++ Engin.py
@@ -6,21 +6,29 @@ import pexpect
 fh = open("story.yaml", mode="r", encoding="utf-8")
 story = yaml.load(fh)
 
-#lpr = subprocess.Popen("/usr/bin/lpr", stdin=subprocess.PIPE)
+#lpr = subprocess.Popen(["/usr/bin/lpr", "-o", "PageCutType=0NoCutPage", "-o", "DocCutType=0NoCutDoc"], stdin=subprocess.PIPE)
 #lpr = pexpect.spawn("/usr/bin/lpr", encoding="utf-8")
-lpr = pexpect.spawn("/usr/bin/lpr")
+#lpr = pexpect.spawn('/usr/bin/lpr')
 
 def print_to_printer(text_to_print):
-    #lpr = subprocess.Popen("/usr/bin/lpr", stdin=subprocess.PIPE)
-    #lpr.stdin.write(text_to_print.encode("utf-8"))
-    #lpr.write(text_to_print.encode("utf-8"))
-    lpr.write(text_to_print)
-    #if lpr.communicate is used, it prints directly when it is called. however you cannot keep the pipe open, and uf you open and close it, the pater gets cut
+    lpr = subprocess.Popen(["/usr/bin/lpr", "-o", "PageCutType=0NoCutPage", "-o", "DocCutType=0NoCutDoc"], stdin=subprocess.PIPE)
+    #lpr.communicate(text_to_print.encode("utf-8"))
+    lpr.communicate(text_to_print.encode("utf-8"))
+    #lpr.write(text_to_print)
+    #lpr.stdin.flush()
+    #if lpr.communicate is used, it prints directly when it is called. however you cannot keep the pipe open, and if you open and close it, the paper gets cut
     #if lpr.stdin.write is used, it prints only when the program is finished, but if you open lpr in the beginning of the process you can print multiple things without the paper being cut
 
-def processState(state):
-    print_to_printer(state["message"])
+def print_to_printer_with_cut(text_to_print):
+    lpr = subprocess.Popen("/usr/bin/lpr", stdin=subprocess.PIPE)
+    lpr.communicate(text_to_print.encode("utf-8"))
 
+def processState(state):
+    if state == story["ende"]:
+        print_to_printer(state["message"])
+        print_to_printer_with_cut(80 * "-")
+    else:
+        print_to_printer(state["message"])
     if not "actions" in state:
         return None
     if "question" in state:
@@ -51,4 +59,4 @@ state = story["start"]
 while state != None:
     state = processState(state)
 
-print_to_printer(80 * "-")
+#print_to_printer(80 * "-")
