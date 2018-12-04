411d61d42b04d39c1583b328743e25e457379390
fio-na <fiona.zurek@posteo.de>
2018-12-04 01:12:43 +0100

    now printing words in the next line if it doesn't fit completely. No Hyphenation (for now) not yet printing empty lines so the full teext is visible while the paper is in the printer

--- Engin.py
+++ Engin.py
@@ -1,24 +1,32 @@
 import yaml
 import subprocess
+#import pyphen
+import textwrap
 
 fh = open("story.yaml", mode="r", encoding="utf-8")
 story = yaml.load(fh)
 
 def send_to_printer(text_to_print):
     #formatted_text = format_text_for_printing(text_to_print.encode("utf-8"))
-    lpr = subprocess.Popen(["/usr/bin/lpr", "-o", "PageCutType=0NoCutPage", "-o", "DocCutType=0NoCutDoc"], stdin=subprocess.PIPE)
-    lpr.communicate(text_to_print.encode("utf-8"))
+    #lpr = subprocess.Popen(["/usr/bin/lpr", "-o", "PageCutType=0NoCutPage", "-o", "DocCutType=0NoCutDoc"], stdin=subprocess.PIPE)
+    formatted_text = textwrap.wrap(text_to_print, 28)
+    for i in formatted_text:
+        lpr = subprocess.Popen(["/usr/bin/lpr", "-o", "PageCutType=0NoCutPage", "-o", "DocCutType=0NoCutDoc"], stdin=subprocess.PIPE)
+        lpr.communicate(i.encode("utf-8"))
 
 def send_to_printer_with_cut(text_to_print):
     #formatted_text = format_text_for_printing(text_to_print.encode("utf-8"))
-    lpr = subprocess.Popen("/usr/bin/lpr", stdin=subprocess.PIPE)
-    lpr.communicate(text_to_print.encode("utf-8"))
+    #lpr = subprocess.Popen("/usr/bin/lpr", stdin=subprocess.PIPE)
+    formatted_text = textwrap.wrap(text_to_print, 28)
+    for i in formatted_text:
+        lpr = subprocess.Popen("/usr/bin/lpr", stdin=subprocess.PIPE)
+        lpr.communicate(i.encode("utf-8"))
 
-def format_text_for_printing(text_to_print):
-    formatting = subprocess.Popen(["/usr/bin/fold", "-s", "-w", "29"])
-    return formatting.communicate(text_to_print)
+#def format_text_for_printing(text_to_print):
+ #   formatting = subprocess.Popen(["/usr/bin/fold", "-s", "-w", "29"])
+  #  return formatting.communicate(text_to_print)
 
-def print_empty_lines():
+#def print_empty_lines():
     for i in range(3):
         lpr = subprocess.Popen(["/usr/bin/lpr", "-o", "PageCutType=0NoCutPage", "-o", "DocCutType=0NoCutDoc"], stdin=subprocess.PIPE)
         lpr.communicate("\n".encode("utf-8"))
@@ -29,16 +37,16 @@ def processState(state):
         send_to_printer_with_cut(80 * "-")
     else:
         send_to_printer(state["message"])
-        print_empty_lines()
+        #print_empty_lines()
     if not "actions" in state:
         return None
     if "question" in state:
         send_to_printer(state["question"])
-        print_empty_lines()
+        #print_empty_lines()
     else: send_to_printer(story["_default_question"])
 
     action = requestAction(state["actions"])
-    print_empty_lines()
+    #print_empty_lines()
     return story[action["next"]]
 
 def requestAction(actions):
