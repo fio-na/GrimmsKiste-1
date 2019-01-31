71365baef15ce446e62c4cbd804c32148bc5f44c
fio-na <fiona.zurek@posteo.de>
2019-01-31 14:52:49 +0100

    commenting the engine for easier reuse, changing hold_time

--- Engin.py
+++ Engin.py
@@ -5,16 +5,18 @@ import textwrap
 import os
 import time
 from gpiozero import Button
-import signal
 
+#writes the pid of a process into a file, which is used by a different script to kill the process, used for restarting
 with open("/tmp/pid.yaml", mode="w", encoding="utf8") as fp:
     yaml.dump(os.getpid(), fp, indent=1)
 
+#use this log to control if the program has restarted or a button has been pushed
 log = open("/home/pi/engine.log", mode="a", encoding="utf8", buffering=1)
 log.write("restart\n")
 
 story = {}
 
+#here you can add new stories, just add add a coma and the path to the story
 filelist = ["/home/pi/GrimmsKiste-1/start.yaml",
             "/home/pi/GrimmsKiste-1/Das-Baby-ist-verschwunden.yaml",
             "/home/pi/GrimmsKiste-1/Das-unerwartete-Feuer.yaml",
@@ -23,13 +25,16 @@ filelist = ["/home/pi/GrimmsKiste-1/start.yaml",
             "/home/pi/GrimmsKiste-1/Nur-mal-kurz-die-Welt-retten.yaml"
             ]
 
+#this pulls the data of the story files into one data structure - story
 for file in filelist:
     with open(file, mode="r", encoding="utf8") as f:
         story_data = yaml.load(f)
         story.update(story_data)
 
-hold_time = 0.2
+#here you can specify the amount of time in seconds a button needs to be pushed, implemented because our buttons got detected to easily
+hold_time = 0.1
 
+#specify the pins (BCM) you use for your buttons
 button1 = Button(5, hold_time=hold_time)
 button2 = Button(6, hold_time=hold_time)
 button3 = Button(13, hold_time=hold_time)
@@ -38,6 +43,7 @@ button4 = Button(19, hold_time=hold_time)
 global start_time
 start_time = time.time() - 1.7
 
+#buttons pushed less then a specified amount of time since the last push will be ignored, because the printer is slower than the program, specify the time in the callbacks
 def check_time_since_last_push_of_a_button():
     global start_time
     push_time = time.time()
@@ -45,6 +51,7 @@ def check_time_since_last_push_of_a_button():
     start_time = push_time
     return secs_since_last_push
 
+#these specify what should happen when a button is pushed, writes it to the log, checks time since last pushed button and if it is greater than specified time changes the state and prints the next part of the story
 def callback_1():
     sec = time.time()
     log.write("{}, : pushed 1\n".format(sec))
@@ -89,16 +96,19 @@ def callback_4():
         else:
             pass
 
+#specifies which callbacks shall be used for which button
 button1.when_held = callback_1
 button2.when_held = callback_2
 button3.when_held = callback_3
 button4.when_held = callback_4
 
+#uses lp to send data to printer, we included options specified for pur printer
 def send_string_to_printer(text_to_print):
     formatted_text = "\n".join(format_text(text_to_print))
     lpr = subprocess.Popen(["/usr/bin/lp", "-o", "PageCutType=0NoCutPage", "-o", "DocCutType=0NoCutDoc"], stdin=subprocess.PIPE)
     lpr.communicate(formatted_text.encode("utf-8"))
 
+#formats all the elements in a list, joins them to one string and prints that string
 def send_list_to_printer(texts_to_print):
     texts_to_print.extend("." for _ in range(8))
     formatted_text = [format_text(i) for i in texts_to_print]
@@ -111,13 +121,16 @@ def send_to_printer_with_cut(text_to_print):
     lpr = subprocess.Popen("/usr/bin/lp", stdin=subprocess.PIPE)
     lpr.communicate(text_to_print.encode("utf-8"))
 
+#formats strings into a list where each element is no longer than 28 characters, doesn't break words, adjust 28 to fit your page size
 def format_text(text_to_print):
     return textwrap.wrap(text_to_print, 28)
 
+#sends a cut to the printer in case the last story wasn't completed, then prints header
 def print_header():
     send_to_printer_with_cut(28 * "-")
     send_string_to_printer("Grimms Kiste".center(84, "-"))
 
+#collects elements which need to be printed into a list
 def print_current_state(current_state):
     list_for_printing = []
     list_for_printing.append(current_state["message"])
@@ -128,21 +141,27 @@ def print_current_state(current_state):
     if "actions" in state:
         list_for_printing.extend(["({}) {}".format(i, action["label"]) for i, action in enumerate(current_state["actions"], start=1)])
     else:
+        #if no actions are in the state, the game is over, therefore the question needs to be deleted and a message that the story is over is appended to the list
         del list_for_printing[-1]
         list_for_printing.append("Damit ist die Geschichte zu Ende.")
     send_list_to_printer(list_for_printing)
 
+#called by the push of a button, collects the information about the next state of the story from the data structure and prints the next state
 def process_state(chosen_action, button):
     global state
     action = chosen_action[button]
     state = story[action["next"]]
     print_current_state(state)
 
+#sets the first state to start, which is the list of stories to choose from
 state = story["start"]
 
+#the actual execution part
 print_header()
 print_current_state(state)
 while "actions" in state:
+    #waiting for buttons being pushed
     time.sleep(0.5)
 send_to_printer_with_cut(28 * "-")
+#giving the reader some time to read the last part
 time.sleep(3.5)
